3777da190644fa01dd1c95631984d3f8cf59963c
Xian Saiful <xiansaiful@gmail.com>
2024-02-24 21:28:03 +0600

    Recap js

---
+++ recap.js
@@ -0,0 +1,8 @@
+/**
+ * 1. var let const
+ * 2. default parameter
+ * 3. template string
+ * 4. arrow function
+ * 5. destructuring and spread operation
+ * 6. for of (used in array, string), for in (used in object)
+ */
