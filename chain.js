0b0715bd1956b90f525bcd575dfbb3633f8657db
Xian Saiful <xiansaiful@gmail.com>
2024-02-25 11:26:53 +0600

    Updated tallest js

---
+++ chain.js
@@ -0,0 +1,20 @@
+// User data access
+const userData = [{
+    id: 01,
+    name: 'Saiful',
+    age: 24,
+    country: 'BD'
+}];
+// console.log(userData[0].name);
+
+// Product list
+const products = {
+    pcount: 5000,
+    data: [
+        {id: 1, pname: 'Asus', price: 5000, color: 'red'},
+        {id: 2, pname: 'Macbook', price: 15000, color: 'black'},
+        {id: 3, pname: 'Dell', price: 75000, color: 'green'},
+    ]
+}
+// get second product price
+// console.log(products.data[1].price);
--- tallest.js
+++ tallest.js
@@ -1,9 +1,13 @@
-const height = [10, 23, 32, 43, 52, 29, 60, 78, 90];
+const height = [10, 23, 32, 43, 52, 29, 60, 78, 90, 500];
 
 function getMaxHeight(numbers) {
+    let maxHeight = 0;
     for(const num of numbers) {
-        console.log(num);
+        if(num > maxHeight) {
+            max = num;
+        }
     }
+    return max;
 }
 const maxHeight = getMaxHeight(height);
-console.log("The max value is: ", maxHeight);
+console.log("The height value is: ", maxHeight); // The height value is:  500
